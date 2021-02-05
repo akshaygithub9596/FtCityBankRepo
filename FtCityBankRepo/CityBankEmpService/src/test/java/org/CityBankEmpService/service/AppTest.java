72ca63ccf6f6f7c5bb9370ca662a3a22119fccac
tl <tl@gmail.com>
2021-02-05 17:56:30 +0530

    Commiting same project again

--- FtCityBankRepo/CityBankEmpService/src/test/java/org/CityBankEmpService/service/AppTest.java
+++ FtCityBankRepo/CityBankEmpService/src/test/java/org/CityBankEmpService/service/AppTest.java
@@ -1,9 +1,5 @@
 package org.CityBankEmpService.service;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
@@ -12,9 +8,7 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	/*
+	 * @Test public void shouldAnswerWithTrue() { assertTrue( true ); }
+	 */
 }
